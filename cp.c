947c500908c08ef235b3b0716fe21204b62da68d
px7nn <px7nnn@gmail.com>
2026-02-02 13:54:12 +0000

    . . .

--- cp.c
+++ cp.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 
+void HandleCP(int argc, char *args[]){
+	printf("Source:\t");
+	for(int i=1; i<argc - 1; i++)
+		printf("\t%s", args[i]);
+	printf("\nDesitination:\t%s\n", args[argc-1]);
+}
+
 int main(int argc, char *args[]){
-	if(argc == 2){
+	if(argc == 1){
+		printf("%s: missing file operand\n", args[0]);
+		return 1;
+	} if(argc == 2){
 		printf("%s: missing destination file operand after '%s'\n", args[0], args[1]);
 		return 1;
+	}
+
+	if(argc >= 3){
+		HandleCP(argc, args);
 	}
 
 	return 0;
