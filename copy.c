f486455a6c56a6a49f20ae05485af6aefa7addc3
px7nn <px7nnn@gmail.com>
2026-02-14 04:21:01 +0530

    Works <src> <dest>

--- copy.c
+++ copy.c
@@ -2,41 +2,38 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 128
-
-void HandleCP(int argc, char *args[]){
-	int source_fd = open(args[1], O_RDONLY);
-	if(source_fd < 0){
-		printf("%s: cannot stat '%s': No such file or directory\n", args[0], args[1]);
-		return;
-	}
-	int target_fd = open(args[argc -1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if(target_fd < 0){
-		printf("Destination error", args[0]);
-		return;
-	}
-	char buffer[BUFFER_SIZE];
-	size_t b_read;
-	while((b_read = read(source_fd, buffer, BUFFER_SIZE)) > 0)
-		write(target_fd, buffer, b_read);
-
-	close(source_fd);
-	close(target_fd);
-}
+int buffer_size = 128;
 
 int main(int argc, char *args[]){
-	if(argc == 1){
-		printf("%s: missing file operand\n", args[0]);
+	if(argc != 3){
+		printf("Usage: %s <source> <destination>");
 		return 1;
-	} if(argc == 2){
-		printf("%s: missing destination file operand after '%s'\n", args[0], args[1]);
+	}
+	
+	int fd_src = open(args[1], O_RDONLY);
+	if(fd_src < 0){
+		perror("Error reading source");
 		return 1;
 	}
 
-	if(argc >= 3){
-		HandleCP(argc, args);
+	int fd_dst = open(args[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if(fd_dst < 0){
+		perror("Error reading source");
+		close(fd_src);
+		return 1;
 	}
 
+	char buffer[buffer_size];
+	ssize_t bytesRead;
+	while((bytesRead = read(fd_src, buffer, buffer_size)) > 0)
+		write(fd_dst, buffer, bytesRead);
+
+	if(bytesRead < 0){
+		perror("Error reading");
+	}
+
+	close(fd_src);
+	close(fd_dst);
 	return 0;
 }
 
